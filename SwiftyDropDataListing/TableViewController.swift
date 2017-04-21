3262c5353461f892b1eb5caede6adf784721e1f4
Sahil <Sahil.beri@aspiringminds.in>
2017-04-21 14:11:00 +0530

    Unused files deleted \n -custom cell file deleted

--- SwiftyDropDataListing/TableViewController.swift
+++ SwiftyDropDataListing/TableViewController.swift
@@ -431,3 +431,36 @@ extension UIImage {
         return image
     }
 }
+
+
+class DropboxListingCell: UITableViewCell {
+  
+  
+  var listImageView: UIImageView = UIImageView()
+  var fileName: UILabel = UILabel()
+  
+  override func awakeFromNib() {
+    
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    self.contentView.addSubview(listImageView)
+    self.contentView.addSubview(fileName)
+    
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    listImageView.frame = CGRect(x: 15, y: 10, width: 30, height: 30)
+    fileName.frame = CGRect(x: (listImageView.frame.origin.x + listImageView.frame.size.width + 10), y: 10, width: UIScreen.main.bounds.size.width-65, height: 30)
+  }
+  
+}
